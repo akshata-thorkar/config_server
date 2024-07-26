5a0314cb2c57d0997aa145373917cfc20471dcf6
akshata-thorkar <144578044+akshata-thorkar@users.noreply.github.com>
2024-07-26 17:47:19 +0530

    Delete assistants directory

--- assistants/src/main/java/com/hirademy/assistants/repository/AssistantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hirademy.assistants.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hirademy.assistants.entity.Assistant;
-
-public interface AssistantRepository extends JpaRepository<Assistant, Long>{
-
-}
