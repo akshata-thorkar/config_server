10d4542cb02861a5a1c4325f4a28df533b5f892c
akshata-thorkar <144578044+akshata-thorkar@users.noreply.github.com>
2024-07-26 17:46:24 +0530

    Delete Service2 directory

--- Service2/src/main/java/com/service/service2/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.service.service2.repository;
-
-import com.service.service2.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
